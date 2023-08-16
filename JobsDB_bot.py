017935b6654f00decffa9eff8991552426fd301d
William Koh <william.jobcontact@gmail.com>
2023-08-16 19:56:55 +0800

    Refactoring the code
    Update the date options and continue with the search job post actions

--- JobsDB_bot.py
+++ JobsDB_bot.py
@@ -12,11 +12,12 @@ import xlsxwriter
 import os
 
 class jobsdbBot:
-    def __init__(self, _domain, _keywords, _locations):
-        global domain, job_keyword, locations, format
+    def __init__(self, _domain, _keywords, _locations, _dates):
+        global domain, job_keyword, locations,dates,format
         domain = _domain
         job_keyword = _keywords
         locations = _locations
+        dates = _dates
         # format = _format 
     
         # ================ Chrome Browser Automation Start ================
@@ -41,7 +42,7 @@ class jobsdbBot:
         search.send_keys(job_keyword)
         search.send_keys(Keys.RETURN)
         driver.find_element(By.CSS_SELECTOR, ".z1s6m00 [data-automation='locationChecklistField']").click()
-        location_names = driver.find_elements(By.CSS_SELECTOR, "div.z1s6m00:not([data-automation]) label.z1s6m00:not([data-automation])")
+        #location_names = driver.find_elements(By.CSS_SELECTOR, "div.z1s6m00:not([data-automation]) label.z1s6m00:not([data-automation])")
         location_btn = driver.find_elements(By.CSS_SELECTOR, "div.z1s6m00 input.z1s6m00")
 
         
@@ -67,6 +68,7 @@ class jobsdbBot:
             'Yau Tsim Mong':19,
             'Yuen Long':20
         }
+        # ================ Select job locations ================
         selected_values = []
         for loc in locations:
             if loc in selectedLocations:
@@ -76,13 +78,73 @@ class jobsdbBot:
 
         for selectBtns in selected_values:
             if selectBtns in range(1, len(location_btn) - 7): #[1-20] are the hk locations
-                location_btn[selectBtns].click()
+                location_btn_element = location_btn[selectBtns]
+                action = ActionChains(driver)
+                action.move_to_element(location_btn_element).click().perform()
                 sleep(1)
             else:
                 print("Selected value is not in the valid range.")
-        
         driver.find_element(By.CSS_SELECTOR, ("button[data-automation='searchSubmitButton']")).click()
         sleep(1)
+
+        # ================ Select dates option================
+        driver.find_element(By.CSS_SELECTOR, ("button[data-automation='createdAtFilterButton']")).click()
+        sleep(1)
+        datesBtn = driver.find_elements(By.CSS_SELECTOR,("div[data-automation='SingleCheckboxGroup']"))
+        selectedDates = {
+            'All':0,
+            '1 day':1,
+            '3 days':2,
+            '7 days':3,
+            '14 days':4
+        }
+        date_values = []
+        for date in dates:
+            if date in selectedDates:
+                selected_dates_value = selectedDates[date] #match the user inputs as the key of the selectedDates and store the related value
+                date_values.append(selected_dates_value) #update the corresponding value to the array
+        print(date_values)
+
+        for selectedBtns in date_values:
+            if selectedBtns:
+                datesBtn[selectedBtns].click()
+                sleep(1)
+            else:
+                print("Selected value is not in the valid range.")
+
+        driver.find_element(By.CSS_SELECTOR, ("button[data-automation='refinementFormApplyButton']")).click()
+        sleep(3)
+
+        # ================ Search jobs one by one================
+        clickJobPosts = driver.find_elements(By.CSS_SELECTOR,"article.z1s6m00")
+        driver.execute_script("window.scrollBy(0,25);")
+        for el in clickJobPosts:
+            try:
+                WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "article.z1s6m00")))
+                WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "article.z1s6m00")))
+                el.click()
+                sleep(0.5)
+            except:
+                print("Element is not clickable or not found.")
+
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div[data-automation='jobDetailsLinkNewTab'] a")))
+
+        page = driver.page_source
+        soup = BeautifulSoup(page, 'lxml')
+        jobs = soup.select("article.z1s6m00")
+
+        companyName = soup.select("article div.z1s6m00 span.z1s6m00 a[data-automation='jobCardCompanyLink']")
+        jobPosition = soup.select("article div.z1s6m00 h1.z1s6m00 span.z1s6m00")
+        jobUrls = soup.select("article.z1s6m00 h1.z1s6m00 a")
+        jobHighlights = soup.select("ul.z1s6m00 li")
+        jobLocation = soup.select("span.z1s6m00 a[data-automation='jobCardLocationLink']")
+        postedDate = soup.select("div.z1s6m00 time span")
+
+
+        print("Numbers of jobs: " + str(len(jobs)))
+        print("=" * 70)
+
               
     """
     output_folder = os.path.join(os.path.dirname(__file__), "output")
@@ -93,40 +155,6 @@ class jobsdbBot:
     columns_name=['職位名稱','公司名稱','地區','工作詳情','發佈時間','網址']
     csv_file = os.path.join(output_folder, f"jobsDB_{job_keyword}_Post.csv")
 
-
-
-    datePostButton = driver.find_element(By.CSS_SELECTOR, ("button[data-automation='createdAtFilterButton']")).click()
-    dates = driver.find_element(By.XPATH,("//span[normalize-space()='Last 7 days']")).click()
-    dateApplyBtn = driver.find_element(By.CSS_SELECTOR, ("button[data-automation='refinementFormApplyButton']")).click()
-    sleep(1)
-    clickJobPosts = driver.find_elements(By.CSS_SELECTOR,"article.z1s6m00")
-    driver.execute_script("window.scrollBy(0,25);")
-    for el in clickJobPosts:
-        try:
-            WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "article.z1s6m00")))
-            WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "article.z1s6m00")))
-            el.click()
-            sleep(0.5)
-        except:
-            print("Element is not clickable or not found.")
-
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div[data-automation='jobDetailsLinkNewTab'] a")))
-
-    page = driver.page_source
-    soup = BeautifulSoup(page, 'lxml')
-    jobs = soup.select("article.z1s6m00")
-
-    companyName = soup.select("article div.z1s6m00 span.z1s6m00 a[data-automation='jobCardCompanyLink']")
-    jobPosition = soup.select("article div.z1s6m00 h1.z1s6m00 span.z1s6m00")
-    jobUrls = soup.select("article.z1s6m00 h1.z1s6m00 a")
-    jobHighlights = soup.select("ul.z1s6m00 li")
-    jobLocation = soup.select("span.z1s6m00 a[data-automation='jobCardLocationLink']")
-    postedDate = soup.select("div.z1s6m00 time span")
-
-
-    print("Numbers of jobs: " + str(len(jobs)))
-    print("=" * 70)
 
     #open the csv file for writing
     with open(csv_file, 'w', newline='',encoding='utf_8_sig') as csvFile:
--- jobsearch.py
+++ jobsearch.py
@@ -2,9 +2,33 @@ from jobsdb_bot import jobsdbBot
 
 domain = "https://hk.jobsdb.com/"
 job_keyword = "programmer" #keyword to search
-locations = ['Eastern','Wan Chai','Kwun Tong','Kwai Tsing','Sai Kung']
+locations = ['Eastern','Wan Chai'] 
+dates = ['1 day']
 #format = "" #excel or csv
 
-bot = jobsdbBot(domain, job_keyword,locations)
+bot = jobsdbBot(domain, job_keyword,locations,dates)
 bot.start()
 #python3 jobsearch.py
+#Max 5 locations 
+"""
+All
+Central & Western
+Cheung Chau
+Eastern
+Kowloon City
+Kwai Tsing
+Kwun Tong
+Lantau Island
+Northern NT
+Sai Kung
+Sham Shui Po
+Shatin
+Southern
+Tai Po
+Tsuen Wan
+Tuen Mun
+Wan Chai
+Wong Tai Sin
+Yau Tsim Mong
+Yuen Long
+"""
